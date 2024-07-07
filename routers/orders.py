b77cf90750813db1be46fb164050a1e5b878bd3d
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-07-07 16:16:16 +0300

    add route delete order

--- routers/orders.py
+++ routers/orders.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Response
 from ..models.models import SessionLocal, Customers, Items, Orders, sa, so
 from ..models import schemas as schemas
 from datetime import date
@@ -37,3 +37,14 @@ def read_order(order_id: int, db: so.Session = Depends(get_db)):
     if db_order is None:
         raise HTTPException(status_code=404, detail="Order not found")
     return db_order
+
+@router.delete("/orders/{order_id}")
+def delete_order(order_id: str, db: so.Session = Depends(get_db)):
+    order_query = db.query(Orders).filter(Orders.order_id == order_id)
+    order = order_query.first()
+    if not order:
+        raise HTTPException(status_code=404,
+                            detail=f'No order with this id: {order_id} found')
+    order_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=204)
