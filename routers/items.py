48fdeedd106990226ecdc5f0499c19659527cc40
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-07-07 15:00:28 +0300

    add routes for orders/item

--- routers/items.py
+++ routers/items.py
@@ -1,13 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from ..models.models import SessionLocal, Customers, Items, Orders, sa
+from ..models import schemas as schemas
+from datetime import date
+
 
 router = APIRouter()
 
-
-@router.get("/items/", tags=["users"])
-async def read_items():
+'''
+@router.get("/customers/", tags=["users"])
+async def read_customers():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
-@router.get("/items/{item_id}", tags=["users"])
-async def read_item(item_id: str):
-    return {"username": item_id}
+@router.get("/customers/{username}", tags=["users"])
+async def read_customer(username: str):
+    return {"username": username}
+'''
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.Item, db: sa.Session = Depends(get_db)):
+    db_item = Items(item_id=item.item_id, item_description=item.item_description, item_price=item.item_price)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+@router.get("/items/", response_model=list[schemas.Items])
+def read_items(skip: int = 0, limit: int = 100, db: sa.Session = Depends(get_db)):
+    return db.query(Items).offset(skip).limit(limit).all()
+
+@router.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: sa.Session = Depends(get_db)):
+    db_item = db.query(Items).filter(Items.item_id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
--- routers/orders.py
+++ routers/orders.py
@@ -1,13 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from ..models.models import SessionLocal, Customers, Items, Orders, sa
+from ..models import schemas as schemas
+from datetime import date
+
 
 router = APIRouter()
 
-
-@router.get("/orders/", tags=["users"])
-async def read_orders():
+'''
+@router.get("/customers/", tags=["users"])
+async def read_customers():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
-@router.get("/orders/{order_id}", tags=["users"])
-async def read_order(order_id: str):
-    return {"username": order_id}
+@router.get("/customers/{username}", tags=["users"])
+async def read_customer(username: str):
+    return {"username": username}
+'''
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+@router.get("/orders/", response_model=list[schemas.Order])
+def read_orders(skip: int = 0, limit: int = 100, db: sa.Session = Depends(get_db)):
+    return db.query(Orders).offset(skip).limit(limit).all()
+
+@router.get("/orders/{order_id}", response_model=schemas.Order)
+def read_order(order_id: int, db: sa.Session = Depends(get_db)):
+    db_order = db.query(Orders).filter(Orders.order_id == order_id).first()
+    if db_order is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_order
