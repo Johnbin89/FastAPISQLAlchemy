578d13992ec7a6b3d722f9414c0298ec1861dadd
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-09-09 15:41:41 +0300

    async explicit begin for customers

--- routers/customers.py
+++ routers/customers.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
-from ..models.models import SessionLocal, Customers, Items, Orders, so, sa
+from fastapi import APIRouter, Depends, HTTPException, status
+from ..models.models import get_db, Customers, Items, Orders, sa, AsyncSession
 from ..models import schemas as schemas
 from datetime import date
 
@@ -17,72 +17,127 @@ async def read_customer(username: str):
     return {"username": username}
 '''
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @router.post("/customers/", response_model=schemas.Customer)
-def create_customer(customer: schemas.Customer, db: so.Session = Depends(get_db)):
+async def create_customer(customer: schemas.Customer, db: AsyncSession = Depends(get_db)):
     db_customer = Customers(cust_id=customer.cust_id, cust_firstname=customer.cust_firstname, cust_lastname=customer.cust_lastname)
-    db.add(db_customer)
-    db.commit()
-    db.refresh(db_customer)
+    async with db.begin():
+        db.add(db_customer)
+        await db.commit()
+    async with db.begin():
+        await db.refresh(db_customer)
     return db_customer
 
 
 @router.get("/customers/", response_model=list[schemas.Customer])
-def read_customers(skip: int = 0, limit: int = 100, db: so.Session = Depends(get_db)):
-    return db.query(Customers).offset(skip).limit(limit).all()
+async def read_customers(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    async with db.begin():
+        stmt = sa.select(Customers).offset(skip).limit(limit)
+        customers = await db.execute(stmt)
+    return customers.scalars().all()
 
 
 @router.get("/customers/{customer_id}", response_model=schemas.Customer)
-def read_customer(customer_id: int, db: so.Session = Depends(get_db)):
-    db_customer = db.query(Customers).filter(Customers.cust_id == customer_id).first()
+async def read_customer(customer_id: int, db: AsyncSession = Depends(get_db)):
+    async with db.begin():
+        stmt = sa.select(Customers).filter(Customers.cust_id == customer_id)
+        db_customer = (await db.execute(stmt)).scalar()
     if db_customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     return db_customer
 
 @router.put("/customers/{customer_id}", response_model=schemas.Customer)
-def edit_customer(customer_id: int, first_name:str = None, last_name :str =None, db: so.Session = Depends(get_db)):
-    db_customer = db.query(Customers).filter(Customers.cust_id == customer_id).first()
-    if db_customer is None:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    
-    if first_name: db_customer.cust_firstname = first_name
-    if last_name: db_customer.cust_lastname = last_name
-    db.commit()
-    db.refresh(db_customer)
+async def edit_customer(customer_id: int, first_name:str = None, last_name :str =None, db: AsyncSession = Depends(get_db)):
+    if not any((first_name, last_name)):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='You should provide first_name or last_name query params')
+    async with db.begin():
+        stmt = sa.select(Customers).filter(Customers.cust_id == customer_id)
+        try:
+            (await db.execute(stmt)).one()
+        except sa.exc.NoResultFound:
+            raise HTTPException(status_code=404, detail="Customer not found")
+    if first_name and last_name:
+        new_values_dict = {'cust_firstname': first_name,
+                           'cust_lastname': last_name}
+    elif first_name:
+        new_values_dict = {'cust_firstname': first_name}
+    elif last_name:
+        new_values_dict = {'cust_lastname': last_name}
+    async with db.begin():
+        stmt = sa.update(Customers).where(Customers.cust_id == customer_id).values(new_values_dict).returning(Customers)
+        db_customer = (await db.execute(stmt)).scalar_one_or_none()
+        await db.commit()
     return db_customer
 
 
 @router.post("/customers/{customer_id}/orders/", response_model=schemas.Order)
-def create_order_for_customer(customer_id: int, item_id: str, date: date, qty: int, db: so.Session = Depends(get_db)):
-    db_customer = db.query(Customers).filter(Customers.cust_id == customer_id).first()
-    if db_customer is None:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    db_item = db.query(Items).filter(Items.item_id == item_id).first()
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    db_order = Orders(order_date=date, order_qty=qty, cust_id=db_customer.cust_id, item_id=db_item.item_id)
-    db.add(db_order)
-    db.commit()
-    db.refresh(db_order)
+async def create_order_for_customer(customer_id: int, item_id: str, date: date, qty: int, db: AsyncSession = Depends(get_db)):
+    async with db.begin():
+        stmt = sa.select(Customers).filter(Customers.cust_id == customer_id)
+        try:
+           (await db.execute(stmt)).one()
+        except sa.exc.NoResultFound:
+            raise HTTPException(status_code=404, detail="Customer not found")
+    
+    async with db.begin():
+        stmt = sa.select(Items).filter(Items.item_id == item_id)
+        try:
+           (await db.execute(stmt)).one()
+        except sa.exc.NoResultFound:
+            raise HTTPException(status_code=404, detail="Item not found")
+ 
+        
+    async with db.begin():
+        stmt = sa.insert(Orders).values({Orders.order_date :date, 
+                                         Orders.order_qty :qty,
+                                         Orders.cust_id: customer_id,
+                                         Orders.item_id: item_id}).returning(Orders)
+        db_order = (await db.execute(stmt)).scalar_one_or_none()
+        await db.commit()
     return db_order
 
 
-
+#4.1
 @router.get("/customers/{customer_id}/orders", response_model=list[schemas.Order])
-def read_customer_orders(cust_id: int, db: so.Session = Depends(get_db)):
-    db_customer = db.query(Customers).filter(Customers.cust_id == cust_id).first()
-    if db_customer is None:
-        raise HTTPException(status_code=404, detail="Customer not found")
-    db_customer_orders = db.query(Orders).filter(Customers.cust_id == db_customer.cust_id).all()
-    if db_customer_orders is None:
+async def read_customer_orders(customer_id: int, db: AsyncSession = Depends(get_db)):
+    async with db.begin():
+        stmt = sa.select(Customers).filter(Customers.cust_id == customer_id)
+        try:
+            (await db.execute(stmt)).one()
+        except sa.exc.NoResultFound:
+            raise HTTPException(status_code=404, detail="Customer not found")
+        
+    async with db.begin():
+        stmt = sa.select(Orders).filter(Orders.cust_id == customer_id)
+        db_customer_orders = (await db.execute(stmt)).scalars().fetchall()
+               
+    if not  db_customer_orders:
         raise HTTPException(status_code=404, detail="Customer has not placed orders yet")
     return db_customer_orders
 
+#4.2
+@router.get("/customers/orders/name")
+async def read_customer_orders_by_name(fname: str | None = None, lname: str | None = None, db: AsyncSession = Depends(get_db)):
+    '''
+    For each order placed by the customer with last_name  or first_name (case insensitive - ilike),
+    find the order id, the quantity ordered and the price of the items he has ordered.
+    '''
+    if fname is None and lname is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Provide First name or Last name at least")
+    if fname and lname:
+        stmt = sa.select(Orders.order_id, Orders.order_qty, Items.item_price)\
+            .join(Items.orders).join(Orders.customer)\
+            .where(Customers.cust_firstname.ilike(fname),
+                   Customers.cust_lastname.ilike(lname))
+    elif fname:
+        stmt = sa.select(Orders.order_id, Orders.order_qty, Items.item_price)\
+            .join(Items.orders).join(Orders.customer)\
+            .where(Customers.cust_firstname.ilike(fname))
+    else:
+        stmt = sa.select(Orders.order_id, Orders.order_qty, Items.item_price)\
+            .join(Items.orders).join(Orders.customer)\
+            .where(Customers.cust_lastname.ilike(lname))
+    async with db.begin(): 
+        result = await db.execute(stmt)
+    return result.mappings().all()   
